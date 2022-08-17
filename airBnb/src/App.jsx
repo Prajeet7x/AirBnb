3874f16095b037b63fa3603829f250f6688d06a2
Prajeet Thakur <np01cp4a190087@islingtoncollege.edu.np>
2022-08-17 09:44:19 +0545

    Props

--- airBnb/src/App.jsx
+++ airBnb/src/App.jsx
@@ -2,13 +2,37 @@ import React from "react"
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Card from "../components/Card"
+import expData from "../Data/data"
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
 
 export default function App() {
-            // <Hero />
+    // <Hero />
+    const items = expData.map((item) => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })
+
     return (
         <div>
             <Navbar />
-            <Card />
+            <section className="cards-list">
+                {items}
+            </section>
         </div>
     )
 }
